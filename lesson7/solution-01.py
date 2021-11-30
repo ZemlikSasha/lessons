aefc401376ad41dcc15c4819a8787d92511ea27d
Alexandr Zemlyanskiy <sasha.zemlyanskiy.00@mail.ru>
2021-11-30 23:17:13 +0300

    add  homework lesson7

--- lesson7/solution-01.py
+++ lesson7/solution-01.py
@@ -13,10 +13,10 @@ def check_coords(x1: int, y1: int, x2: int, y2: int) -> bool:
 
 if __name__ == "__main__":
     x1 = int(input("Enter X1:"))
-    y1 = int(input("Enter X1:"))
+    y1 = int(input("Enter Y1:"))
 
-    x2 = int(input("Enter X1:" ))
-    y2= int(input("Enter X1:"))
+    x2 = int(input("Enter X2:"))
+    y2 = int(input("Enter Y2:"))
 
     if check_coords(x1, y1, x2, y2):
        print("Yes")
