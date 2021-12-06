c93b0d2ae2bf280ad27beea56444efa1404fba3b
Alexandr Zemlyanskiy <sasha.zemlyanskiy.00@mail.ru>
2021-12-06 10:21:28 +0300

    add classwork 7

---
+++ lesson8/solution-01.py
@@ -0,0 +1,34 @@
+"""
+Дан список словарей. Каждый словарь описывает машину (серийный номер, цвет и год выпуска). Создать функцию,
+которая возвращает новый список со всеми машинами, год выпуска которых больше Х.
+"""
+
+
+def filter_cars(car_list: list, year: int) -> list:
+    result = []
+    for car in car_list:
+        if car["year"] < year:
+            result.append(car)
+    return result
+
+
+if __name__ == "__main__":
+    car_list = [
+    {
+        "serial": 123456,
+        "color": "red",
+        "year": 1999,
+    },
+    {
+        "serial": 234567,
+        "color": "black",
+        "year": 2020,
+    },
+    {
+        "serial": 345678,
+        "color": "white",
+        "year": 2012,
+    },
+]
+    year = int(input("Year:"))
+    print(filter_cars(car_list, year))
