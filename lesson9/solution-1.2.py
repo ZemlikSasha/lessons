8b5baa1e3003e9bcf7da1ee4274b4ba343f42b02
Alexandr Zemlyanskiy <sasha.zemlyanskiy.00@mail.ru>
2021-12-07 22:36:47 +0300

    add homework 9

---
+++ lesson9/solution-1.2.py
@@ -0,0 +1,24 @@
+"""
+Создать программу, которая импортирует класс из предыдущей задачи, создает объект и при инициализации устанавливает
+марку Mercedes, модель E500, год выпуска 2000. Далее “разгоняет” машину до 100 км/ч и выводит скорость на экран.
+"""
+
+
+from solution-1.1 import Car
+
+def fast_speed(car):
+    while car.speed < 100:
+        car.increase_speed()
+    car.show_speed()
+    return
+
+if __name__ == "__main__":
+    car = Car("Mersedes", "E500", 2000, 0)
+
+    fast_speed(car)
+    print(car.brand)
+    print(car.model)
+    print(car.year)
+    print(car.speed)
+
+
